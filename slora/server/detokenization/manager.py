66f3eb42f797fafaa346257c8f3b3f7ea31d2896
Yi-Han Ding <yd3163@b-9-46.c.hpc-slurm-9c75.internal>
2025-11-16 17:23:58 -0500

    setup basic env

--- slora/server/detokenization/manager.py
+++ slora/server/detokenization/manager.py
@@ -85,6 +85,11 @@ def start_detokenization_process(args, detokenization_port, httpserver_port, pip
         pipe_writer.send(str(e))
         raise
     pipe_writer.send('init ok')
-    loop = asyncio.get_event_loop()
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    # loop = asyncio.get_event_loop()
     loop.run_until_complete(router.handle_loop())
     return
